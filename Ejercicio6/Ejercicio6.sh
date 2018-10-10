6a7be1fd73aa6d5ed03d40ae76a1bd9994f55656
Nicolas Sapaya <nicolas.sapaya@gmail.com>
2018-10-10 16:57:12 +0000

    help

--- Ejercicio6/Ejercicio6.sh
+++ Ejercicio6/Ejercicio6.sh
@@ -17,6 +17,39 @@
 #							#
 #################################################
 
+#	Funcion si se ingresaro parametros incorrectos.
+ErrorSintaxOHelp() { 
+	clear
+	#	Si $1 es 1, entonces es error de sintaxis
+	if test $1 != 0; then
+		echo 'Error! La sintaxis del script es la siguiente:'
+		echo ""
+	else
+		echo 'Help:'
+		echo "La sintaxis del script es la siguiente:"
+		echo ""
+	fi
+	echo "Ejemplo:"
+	echo "$0" '"path" ".txt"'
+	echo ""
+	echo 'Para ayuda:'
+	echo "$0 -h"
+	echo "$0 -?"
+	echo "$0 -help"
+	exit	
+}
+
+#	Es ayuda ?
+if (test $# -eq 1); then 
+	if [ "$1" = "-h" ]; then
+		ErrorSintaxOHelp 0
+	elif [ "$1" = "-help" ]; then
+		ErrorSintaxOHelp 0
+	elif [ "$1" = "-?" ]; then
+		ErrorSintaxOHelp 0
+	fi
+fi
+
 inotifywait -e modify,create,delete -r $1 -q -m |
 
 while read path action file; do
