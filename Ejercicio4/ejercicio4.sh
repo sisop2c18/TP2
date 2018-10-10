cf6e4fedd72c543bab2055c871c5b32e1688fb5c
federicoSchafer <31144638+federicoSchafer@users.noreply.github.com>
2018-10-10 12:20:36 -0300

    Ejercicio4

---
+++ Ejercicio4/ejercicio4.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+arch="$1"
+
+function signal_SIGUSR1(){ 
+	echo "********************************************************************************************" >> "$arch"
+	echo -e "El usuario $(whoami) realizo el evento SIGUSR1. Ocurrio el dia: $(date +"%d-%m-%y") a las $(date +"%T")" >> "$arch"
+	free -m | awk 'NR==2 {porcentaje=$4/$2 * 100; print "\nPorcentaje de memoria libre: " porcentaje"%"}' >> "$arch"
+	free -m | awk 'NR==2 {porcentaje=$3/$2 * 100; print "\nPorcentaje de memoria usada: " porcentaje"%"}' >> "$arch"
+}
+function signal_SIGUSR2(){
+	echo "********************************************************************************************" >> "$arch"
+	echo -e "El usuario $(whoami) realizo el evento SIGUSR2. Ocurrio el dia: $(date +"%d-%m-%y") a las $(date +"%T")" >> "$arch"
+	echo -e "\nEl estado de los filesystem: \n$(df -Th)" >> "$arch"
+}
+function signal_SIGTERM(){
+	echo "********************************************************************************************" >> "$arch"
+	echo -e "El proceso finalizo el dia: $(date +"%d-%m-%y") a las $(date +"%T")" >> "$arch"
+	exit
+}
+
+echo  El numero de proceso es: $$
+trap '' SIGINT 
+
+trap signal_SIGUSR1 10
+
+trap signal_SIGUSR2 12
+
+trap signal_SIGTERM 15
+
+while true; do
+	sleep 1
+done
---
+++ Ejercicio4/lanzadorEjercicio4.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+function ayuda(){
+	echo "NAME"
+	echo "   Ejercicio4.sh"
+	echo " "
+	echo "SYNOPSIS"
+	echo "  ./LanzadorEjercicio4.sh ./archivo.log "
+	echo "	kill -s SIGUSR1 nroProceso"
+	echo "	kill -s SIGUSR2 nroProceso"
+	echo "	kill -s SIGTERM nroProceso"
+	echo " "
+	echo "DESCRIPTION"
+	echo "El script al recibir una seañal SIGURS1 muestra por pantalla Nombre del usuario, fecha y hora del evento (timestamp), porcentaje de uso de la CPU, memoria utilizada y disponible; al recibir una señal SIGURS2 muestra Nombre del usuario, fecha y hora del evento (timestamp), información sobre el estado de utilización de todos los file systems que tenga el equipo. El script termina su ejecucion con una señal SIGTERM y registra la hora de finalización del proceso y termina." 
+
+}
+
+if [ $# == 0 ];then
+	echo "La cantidad de parametros ingresados es incorrecto."
+	exit
+fi
+
+if [ $# == 1 ];then
+	if test "$1" = "-h" -o "$1" = "-?" -o "$1" = "-help";then
+		ayuda
+		exit
+	fi
+fi
+
+if [ $# -ge 2 ];then
+	echo "La cantidad de parametros ingresados es incorrecto."
+	exit
+fi
+
+if [ ! -f "$1" ];then
+	echo "El archivo de log no existe, por lo tanto se creo"
+	touch "$1"
+fi
+
+echo "********************************************************************************************" >> "$1"
+echo "El proceso comienza el $(date +"%d-%m-%y") a las $(date +"%T")" >> "$1"
+bash "$PWD"/ejercicio4.sh "$1" &
